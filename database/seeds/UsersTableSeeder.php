19c0893c09a8540bfbd45654ecd2134519f9cf20
Paul F. Rubio <pr344@njit.edu>
2018-03-24 17:07:13 -0400

    3.  Create a database seed that inserts 50 users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //To use the factory model:
+        factory(App\User::class,50)->create()->each(function ($u) {
+            // $u->posts()->save(factory(App\Post::class)->make());
+        });
+    }
+}
