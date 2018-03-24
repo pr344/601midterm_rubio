9940d58e083a620d3742b0472b5628395eb51d7c
Paul F. Rubio <pr344@njit.edu>
2018-03-24 18:10:47 -0400

    5.2  Update to the correct faker type [] for array

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -4,8 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Car::class, function (Faker $faker) {
     return [
-        'Make' => $faker->randomElement('Ford','Honda','Toyota'),
-        'Model' => $faker->randomElement('Coupe','Sedan','SUV','Pickup'),
+        'Make' => $faker->randomElement(['Ford','Honda','Toyota']),
+        'Model' => $faker->randomElement(['Coupe','Sedan','SUV','Pickup']),
         'Year' => $faker->numberBetween(1980,2018),
     ];
 });
