0e9cc4a7df557425f6987936f5f0cdaa7a158de4
Paul F. Rubio <pr344@njit.edu>
2018-03-24 20:35:34 -0400

    9  1.Write a feature test to check if the register return a 200 status code.

---
+++ tests/Feature/PagesTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PagesTest extends TestCase
+{
+
+    //test register page is working
+    public function testRegisterScreen()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+}
