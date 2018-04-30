ec5254d213666dde9ffb87ba8b4d2dac5c5d4511
Paul F. Rubio <pr344@njit.edu>
2018-04-30 19:25:13 -0400

    Added the ability to edit a profile

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ Route::get('/home', 'PagesController@home')->name('home');
 Route::get('/user/{user_id}/profile', 'ProfileController@create')->name('profile.create');
 Route::post('/user/{user_id}/profile/', 'ProfileController@store')->name('profile.store');
 Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
+Route::get('/user/{user_id}/profile/{profile_id}/edit', 'ProfileController@edit')->name('profile.edit');
+Route::patch('/user/{user_id}/profile/{profile_id}', 'ProfileController@update')->name('profile.update');
+
 
 Auth::routes();
 
